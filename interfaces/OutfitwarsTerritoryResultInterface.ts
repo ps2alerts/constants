5d21309f14e29674eea73ac957a910193e9381a4
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-11 22:16:57 +0100

    Changed from team1 etc to use blue vs red. Updated CreateInstanceOutfitWarsDto and associated entity

--- interfaces/OutfitwarsTerritoryResultInterface.ts
+++ interfaces/OutfitwarsTerritoryResultInterface.ts
@@ -3,6 +3,6 @@ import {Team} from '../outfitwars/team';
 
 export interface OutfitwarsTerritoryResultInterface extends TerritoryResultInterface {
     victor: Team;
-    team1: number;
-    team2: number;
+    blue: number;
+    red: number;
 }
