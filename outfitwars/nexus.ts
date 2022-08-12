363b06d5f97cb59e58f36115fcbb7d66319956e3
Ryan <ryanjsims@email.arizona.edu>
2022-08-12 01:01:27 -0700

    Add nexus initial map data

---
+++ outfitwars/nexus.ts
@@ -0,0 +1,120 @@
+export const NexusInitialMapData = [
+    {
+        "ZoneId": "10",
+        "Regions": {
+            "IsList": "1",
+            "Row": [
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310500",
+                            "facility_type_id": "Arazek SATCOM Hub",
+                            "facility_name": "5"
+                        },
+                        "FactionId": "0"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310510",
+                            "facility_type_id": "Nexus Secure Substation",
+                            "facility_name": "6"
+                        },
+                        "FactionId": "3"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310520",
+                            "facility_type_id": "Hydroponics Compound",
+                            "facility_name": "6"
+                        },
+                        "FactionId": "2"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310530",
+                            "facility_type_id": "The Granitehead Reactor",
+                            "facility_name": "5"
+                        },
+                        "FactionId": "0"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310540",
+                            "facility_type_id": "Argent Pass",
+                            "facility_name": "6"
+                        },
+                        "FactionId": "3"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310550",
+                            "facility_type_id": "Slate Canyon Hold",
+                            "facility_name": "6"
+                        },
+                        "FactionId": "2"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310560",
+                            "facility_type_id": "Nexus Northeast Warpgate",
+                            "facility_name": "7"
+                        },
+                        "FactionId": "2"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310570",
+                            "facility_type_id": "Nexus Southwest Warpgate",
+                            "facility_name": "7"
+                        },
+                        "FactionId": "3"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310590",
+                            "facility_type_id": "Bitter Gorge Garrison",
+                            "facility_name": "5"
+                        },
+                        "FactionId": "0"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310600",
+                            "facility_type_id": "Nexus Alpha",
+                            "facility_name": "5"
+                        },
+                        "FactionId": "3"
+                    }
+                },
+                {
+                    "RowData": {
+                        "map_region": {
+                            "facility_id": "310610",
+                            "facility_type_id": "Nexus Omega",
+                            "facility_name": "5"
+                        },
+                        "FactionId": "2"
+                    }
+                }
+            ]
+        }
+    }
+];
