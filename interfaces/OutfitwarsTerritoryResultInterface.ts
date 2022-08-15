cf300f4c03974c15dc3216f9b53a037b59de8687
Ryan <ryanjsims@email.arizona.edu>
2022-08-14 21:02:13 -0700

    Make it possible for victor to be null

--- interfaces/OutfitwarsTerritoryResultInterface.ts
+++ interfaces/OutfitwarsTerritoryResultInterface.ts
@@ -2,7 +2,7 @@ import TerritoryResultInterface from './TerritoryResultInterface';
 import {Team} from '../outfitwars/team';
 
 export interface OutfitwarsTerritoryResultInterface extends TerritoryResultInterface {
-    victor: Team;
+    victor: Team | null;
     blue: number;
     red: number;
 }
