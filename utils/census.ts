92e2dfd3587c908566c3c81365cee96965ec60e5
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-24 12:29:03 +0100

    Added new util folder and added getCensusBaseUrl to shorten query code

---
+++ utils/census.ts
@@ -0,0 +1,7 @@
+import {CensusEnvironment} from '../censusEnvironments';
+
+const getCensusBaseUrl = (serviceId: string, environment: CensusEnvironment): string => {
+    return `https://census.daybreakgames.com/s:${serviceId}/get/${environment}`;
+};
+
+export default getCensusBaseUrl;
