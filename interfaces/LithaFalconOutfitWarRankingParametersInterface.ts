a1e75e044c66dd977991133c26ec39137e5287da
Ryan Sims <ryanjsims@email.arizona.edu>
2022-08-24 18:45:07 -0700

    Update falcon outfit wars interface

--- interfaces/LithaFalconOutfitWarRankingParametersInterface.ts
+++ interfaces/LithaFalconOutfitWarRankingParametersInterface.ts
@@ -2,10 +2,13 @@
 export default interface LithaFalconOutfitWarRankingParametersInterface {
     TotalScore: number,
     MatchesPlayed: number,
-    VictoryPoints: number,
-    Gold: number,
-    Silver: number,
-    Bronze: number,
+    VictoryPoints?: number,
+    Gold?: number,
+    Silver?: number,
+    Bronze?: number,
+    Wins?: number,
+    Losses?: number,
+    TiebreakerPoints?: number,
     FactionRank: number,
     GlobalRank: number
 }
