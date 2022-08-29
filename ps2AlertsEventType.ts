227ee6960763482669590c5c2f9a6cc5df6ba125
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-29 18:28:40 +0100

    Changed enum lettering to match database style

---
+++ ps2AlertsEventType.ts
@@ -0,0 +1,6 @@
+export enum Ps2AlertsEventType {
+    LIVE_METAGAME = 1,
+    OUTFIT_WARS_AUG_2022 = 2,
+}
+
+export const ps2AlertsEventTypeArray = [1, 2];
--- ps2alertsEventType.ts
+++
@@ -1,6 +0,0 @@
-export enum Ps2alertsEventType {
-    LIVE_METAGAME = 1,
-    OUTFIT_WARS_AUG_2022 = 2,
-}
-
-export const ps2alertsEventTypeArray = [1, 2];
