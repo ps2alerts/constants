4cd24ec250fab878a9e53dab189e82151f47b4a2
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-03 15:30:28 +0100

    Improve formatting of brackets to signify team colours and showing team faction labels

--- interfaces/OutfitwarsRankingInterface.ts
+++ interfaces/OutfitwarsRankingInterface.ts
@@ -1,25 +1,25 @@
-import {World} from '../world';
-import LithaFalconOutfitWarRankingParametersInterface from './LithaFalconOutfitWarRankingParametersInterface';
-import {Faction} from '../faction';
+import { World } from '../world'
+import { Faction } from '../faction'
+import LithaFalconOutfitWarRankingParametersInterface from './LithaFalconOutfitWarRankingParametersInterface'
 
 export interface OutfitwarsOutfitDataInterface {
-    id: string;
-    name: string;
-    faction: Faction;
-    world: World;
-    leader: string;
-    tag: string | null;
+    id: string
+    name: string
+    faction: Faction
+    world: World
+    leader: string
+    tag: string | null
 }
 
 export interface OutfitwarsRankingInterface {
-    timestamp: Date;
-    startTime: Date;
-    round: number;
-    world: World;
-    outfitWarId: number;
-    roundId: string;
-    order: number;
-    outfit: OutfitwarsOutfitDataInterface;
-    rankingParameters: LithaFalconOutfitWarRankingParametersInterface;
-    instanceId: string | null;
+    timestamp: Date
+    startTime: Date
+    round: number
+    world: World
+    outfitWarId: number
+    roundId: string
+    order: number
+    outfit: OutfitwarsOutfitDataInterface
+    rankingParameters: LithaFalconOutfitWarRankingParametersInterface
+    instanceId: string | null
 }
