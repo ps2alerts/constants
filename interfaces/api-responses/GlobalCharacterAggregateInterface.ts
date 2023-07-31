5bf04559b0ddf80a56b86f1a10a3c9e8741140bf
Matt Cavanagh <maelstrome26@gmail.com>
2023-07-31 15:01:24 +0100

    Fixed relative paths (again)

--- interfaces/api-responses/GlobalCharacterAggregateInterface.ts
+++ interfaces/api-responses/GlobalCharacterAggregateInterface.ts
@@ -1,8 +1,8 @@
 import { PS2AlertsFactionKillDataInterface } from "../PS2AlertsFactionKillDataInterface";
 import { World } from "../../world";
-import { Bracket } from "~/ps2alerts-constants/bracket";
-import { Ps2AlertsEventType } from "~/ps2alerts-constants/ps2AlertsEventType";
-import { PS2AlertsCharacterInterface } from "~/ps2alerts-constants/interfaces/PS2AlertsCharacterInterface";
+import { Bracket } from "../../bracket";
+import { Ps2AlertsEventType } from "../../ps2AlertsEventType";
+import { PS2AlertsCharacterInterface } from "../../interfaces/PS2AlertsCharacterInterface";
 
 export interface GlobalCharacterAggregateInterface {
     world: World;
